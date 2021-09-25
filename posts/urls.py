74e68c74bf4aefb60145be34e1debd46846524af
Mustafa <eng.mustafaelagamey@gmail.com>
2021-09-25 18:38:11 +0200

    Add posts list

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.latest),
+    path('posts', views.listing),
 ]
--- posts/views.py
+++ posts/views.py
@@ -10,4 +10,11 @@ def latest(request):
     context = {
         'posts': posts_list[:3]
     }
-    return render(request,'posts/latest.html',context)
+    return render(request, 'posts/latest.html', context)
+
+
+def listing(request):
+    context = {
+        'posts': posts_list[:4]
+    }
+    return render(request, 'posts/list.html', context)
