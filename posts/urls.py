26e26d0ad799b886af2f53107ac94b0c70c94d3d
Mustafa <eng.mustafaelagamey@gmail.com>
2021-09-25 19:02:05 +0200

    Add some dynamic links

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
 urlpatterns = [
-    path('', views.latest),
-    path('posts', views.listing),
-    path('post/<title>', views.view),
+    path('', views.latest, name='latest'),
+    path('posts', views.listing, name='listing'),
+    path('post/<title>', views.view, name='view'),
 ]
