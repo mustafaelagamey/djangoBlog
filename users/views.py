35cdfd607c9320ccdb89c9dd622c059a6285138a
Mustafa <eng.mustafaelagamey@gmail.com>
2021-11-12 15:05:51 +0200

    Feat: Show profiles list

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Profile
 
 
-class ProfileListView(TemplateView):
+class ProfileListView(ListView):
     template_name = 'users/profile/list.html'
+    model = Profile
